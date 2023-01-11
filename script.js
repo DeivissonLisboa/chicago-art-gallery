6302a9b98f10fc30ea76b8f167e231c8b2512125
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-11 01:40:59 -0300

    Create nav highlight function

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
+const title = document.getElementById("title")
+const letters = title.innerText.split("")
+const icons = document.querySelectorAll(".top-nav i")
 const cards = document.querySelectorAll(".card")
 
+title.innerText = ""
+
+letters.forEach((letter) => {
+  let span = document.createElement("span")
+  span.innerText = letter
+  span.classList.add("title-letter")
+  title.appendChild(span)
+})
+
+const spans = document.querySelectorAll(".title-letter")
+
+function checkSpanHitbox(card = document.querySelector(".card.active")) {
+  spans.forEach((span) => {
+    const spanX = span.offsetLeft
+    const cardX = card.offsetLeft
+    const cardWidth = card.offsetWidth
+
+    if (spanX + 10 >= cardX && spanX <= cardX + cardWidth) {
+      span.classList.add("highlight")
+    } else {
+      span.classList.remove("highlight")
+    }
+  })
+  icons.forEach((icon) => {
+    const iconX = icon.offsetLeft
+    const cardX = card.offsetLeft
+    const cardWidth = card.offsetWidth
+
+    if (iconX + 10 >= cardX && iconX <= cardX + cardWidth) {
+      icon.classList.add("highlight")
+    } else {
+      icon.classList.remove("highlight")
+    }
+  })
+}
+
 cards.forEach((card, i) => {
+  if (i === 0) {
+    card.classList.add("active")
+  }
+
   card.addEventListener("mouseover", () => {
-    cards.forEach((i) => {
-      i.classList.remove("active")
+    cards.forEach((j) => {
+      j.classList.remove("active")
     })
 
     card.classList.add("active")
+
+    checkSpanHitbox(card)
   })
 })
+
+checkSpanHitbox()
